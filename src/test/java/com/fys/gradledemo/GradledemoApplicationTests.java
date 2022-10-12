24668d27b073cc24ddbf1c980b0e1349651e1883
fuyuanshun <fuyuanshun521@gmail.com>
2022-10-12 15:35:29 +0800

    Initial commit

---
+++ src/test/java/com/fys/gradledemo/GradledemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fys.gradledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradledemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
